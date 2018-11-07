c5cd699bb831c561cf7816652f8792a483c69647
Zoran Felbar <zoran.felbar@gmail.com>
2018-11-07 17:05:06 +0100

    Code refectoring, fixes

--- js/experiment.js
+++ js/experiment.js
@@ -1,18 +1,48 @@
 (function ($) {
     $.fn.experiment = function (options) {
-        var defaults = {};
-        var settings = Object.assign(defaults, options);
+        var defaults = {
+            name: "John Doe",
+            currentColor: "orange",
+            primaryColor: "red",
+            secondaryColor: "green"
+        };
+        var settings = {};
 
-        this.setName = function(name) {}
+        this.init = function() {
+            Object.assign(defaults, options);
+            settings = Object.assign({...defaults}, {...options});
+            this.setName(settings.name);
+            this.setColor(settings.currentColor);
+        };
 
-        this.setColor = function(color) {}
+        this.setName = function(name) {
+            this.text(`Hello ${ name }. Click me!`);
+            settings.name = name;
+        }
 
-        this.colorToggle = function() {}
+        this.setColor = function(color) {
+            this.css("color", color);
+            settings.currentColor = color;
+        }
 
-        this.reset = function() {}
+        this.colorToggle = function() {
+            if (settings.currentColor === settings.primaryColor) {
+                this.setColor(settings.secondaryColor)
+            } else {
+                this.setColor(settings.primaryColor)
+            }
+        }
 
-        this.setName(settings.name);
-        this.setColor(settings.color.current);
+        this.reset = function() {
+            this.setName(defaults.name);
+            this.setColor(defaults.currentColor);
+        }
+
+        this.getSettings = function() {
+            return settings;
+        }
+
+        this.init(this);
 
         return this;
     };
